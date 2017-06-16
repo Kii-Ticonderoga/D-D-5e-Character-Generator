309be240e7e10694c81dc2bb09ebc9fd3892f78a
mbmorgan <morgan.michaelb@gmail.com>
2017-06-16 14:14:36 -0600

    Update statGenerator.rb

--- statGenerator.rb
+++ statGenerator.rb
@@ -1,6 +1,4 @@
-sNames = {"STR":,"DEX":,"CON":,"INT":,"WIS":,"CHA":}
-
-
-sNames.each { |i| sNames[i] => rand(3..18) }
-
-puts sNames
+stats = {"STR"=>[nil,nil],"DEX"=>[nil,nil],"CON"=>[nil,nil],"INT"=>[nil,nil],"WIS"=>[nil,nil],"CHA"=>[nil,nil]}
+stats.keys.each { |i| stats[i][0]=rand(6..18)
+stats[i][1] = (stats[i][0]-10)/2.to_i }
+stats.each { |k,v| puts "#{k} = " + v[0].to_s + ", " + (v[1].to_i>0?"+":"") + v[1].to_s }
